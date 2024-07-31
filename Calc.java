7b0843bba61a599163fe2545591e35c6bbc492b0
danielhrqs4@yahoo.com <danielhrqs4@yahoo.com>
2024-07-31 13:19:27 -0300

    c1

---
+++ Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static void main(String[] args) {
+        System.out.println(soma(1,2));    
+    }
+
+    public static int soma(int a , int b){
+        return a+b;
+    }
+}
